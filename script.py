d16e1e1151eaee6f88c643b1ead25386ad1f32e4
Michał Kozioł <97291619+mkoziol647@users.noreply.github.com>
2023-12-22 15:48:25 +0100

    Create script.py

---
+++ script.py
@@ -0,0 +1,45 @@
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_two_dollar_slices = prices.count(2)
+print(num_two_dollar_slices)
+
+print("----------")
+
+num_pizzas = len(toppings)
+print("We sell " + str(num_pizzas) + " different kinds of pizza!")
+
+print("----------")
+
+pizza_and_prices = [[2, "pepperoni"], [6, "pineapple"], [1, "cheese"], [3, "sausage"], [2, "olives"], [7, "anchovies"], [2, "mushrooms"]]
+print(pizza_and_prices)
+
+print("----------")
+
+pizza_and_prices.sort()
+print(pizza_and_prices)
+
+print("----------")
+
+cheapest_pizza = pizza_and_prices[0]
+print(cheapest_pizza)
+
+print("----------")
+
+priciest_pizza = pizza_and_prices[-1]
+print(priciest_pizza)
+
+print("----------")
+
+pizza_and_prices.pop(-1)
+print(pizza_and_prices)
+
+print("----------")
+
+pizza_and_prices.insert(4, [2.5, "peppers"])
+print(pizza_and_prices)
+
+print("----------")
+
+three_cheapest = pizza_and_prices[:3]
+print(three_cheapest)
